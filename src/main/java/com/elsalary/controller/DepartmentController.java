1391e60783970bed1c3dbcda2eadc608126b6f7d
rishabh srivastav <srivastav.rishabh@gmail.com>
2020-04-23 02:24:30 +0530

    department tested

--- src/main/java/com/elsalary/controller/DepartmentController.java
+++ src/main/java/com/elsalary/controller/DepartmentController.java
@@ -3,10 +3,7 @@ package com.elsalary.controller;
 import com.elsalary.model.Department;
 import com.elsalary.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -16,7 +13,7 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
     @RequestMapping(value = "/department", method = RequestMethod.POST)
-    public Department create(Department department) {
+    public Department create(@RequestBody Department department) {
         return departmentService.createDepartment(department);
     }
 }
--- src/main/java/com/elsalary/model/Department.java
+++ src/main/java/com/elsalary/model/Department.java
@@ -2,16 +2,13 @@ package com.elsalary.model;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 public class Department {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private long id;
     @Column
     private String name;
