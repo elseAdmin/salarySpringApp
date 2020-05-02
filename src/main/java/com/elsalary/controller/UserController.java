5ba8470221b76b8ef32ba4f6ab34fab421da2dc9
rishabh srivastav <srivastav.rishabh@gmail.com>
2020-05-03 02:26:44 +0530

    User detail fetched from username

--- src/main/java/com/elsalary/controller/UserController.java
+++ src/main/java/com/elsalary/controller/UserController.java
@@ -1,12 +1,23 @@
 package com.elsalary.controller;
 
+import com.elsalary.config.TokenProvider;
+import com.elsalary.constants.ApplicationConstants;
 import com.elsalary.model.User;
 import com.elsalary.model.Request.UserDto;
+import com.elsalary.model.response.UserResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 import com.elsalary.service.UserService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -16,10 +27,23 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private TokenProvider jwtTokenUtil;
+
     @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public List listUser() {
+    public List<User> listUser() {
         return userService.findAll();
+    }
+
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @RequestMapping(value = "user/getId/{username}", method = RequestMethod.GET)
+    public ResponseEntity getUser(@PathVariable String username) throws IOException {
+        User user = userService.findOne(username);
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setId(user.getId());
+        userResponseDto.setUsername(user.getUsername());
+        return new ResponseEntity(userResponseDto, HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
